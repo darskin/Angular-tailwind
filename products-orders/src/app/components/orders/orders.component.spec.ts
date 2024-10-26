fc11a0ac6028bf4a045584e0fc563981f01c066c
Esteban <esteban.buitrago.o@gmail.com>
2024-10-25 23:25:06 -0500

    implement docker and unit test

--- products-orders/src/app/components/orders/orders.component.spec.ts
+++ products-orders/src/app/components/orders/orders.component.spec.ts
@@ -1,17 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OrdersComponent } from './orders.component';
+import { OrderService } from '../../services/order.service';
+import { ProductService } from '../../services/product.service';
+import { MatDialogModule } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { OrderData } from '../order-modal/order-modal.component';
 
 describe('OrdersComponent', () => {
   let component: OrdersComponent;
   let fixture: ComponentFixture<OrdersComponent>;
+  let orderServiceMock: any;
+  let productServiceMock: any;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [OrdersComponent]
-    })
-    .compileComponents();
+    orderServiceMock = {
+      orders$: of([]),
+      updateOrder: jasmine.createSpy('updateOrder').and.returnValue(of({})),
+      createOrder: jasmine.createSpy('createOrder').and.returnValue(of({})),
+      deleteOrder: jasmine.createSpy('deleteOrder').and.returnValue(of({}))
+    };
 
+    productServiceMock = {
+      getProducts: jasmine.createSpy('getProducts').and.returnValue(of([]))
+    };
+
+    await TestBed.configureTestingModule({
+      imports: [MatDialogModule, OrdersComponent], // Importar el componente standalone
+      providers: [
+        { provide: OrderService, useValue: orderServiceMock },
+        { provide: ProductService, useValue: productServiceMock }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(OrdersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +41,32 @@ describe('OrdersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load orders on init', () => {
+    expect(orderServiceMock.orders$).toBeTruthy();
+  });
+
+  it('should load products on init', () => {
+    expect(productServiceMock.getProducts).toHaveBeenCalled();
+  });
+
+  it('should calculate total correctly', () => {
+    const order: OrderData = {
+      id: 1,
+      customerId: 1,
+      date: '2023-01-01',
+      items: [
+        { productId: 1, quantity: 2, price: 10 },
+        { productId: 2, quantity: 1, price: 20 }
+      ]
+    };
+    expect(component.getTotal(order)).toBe(40);
+  });
+
+  it('should return product name', () => {
+    component.products = [{ id: 1, name: 'Product A', description: '', price: 0, stock: 0 }];
+    expect(component.getProductName(1)).toBe('Product A');
+    expect(component.getProductName(2)).toBe('Producto no encontrado');
   });
 });
--- products-orders/src/app/components/products/products.component.spec.ts
+++ products-orders/src/app/components/products/products.component.spec.ts
@@ -1,17 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { ProductsComponent } from './products.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ProductService } from '../../services/product.service';
+import { of } from 'rxjs';
+import { ProductModalComponent } from '../product-modal/product-modal.component';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let mockProductService: any;
+  let mockDialog: any;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProductsComponent]
-    })
-    .compileComponents();
+    mockProductService = {
+      products$: of([]),
+      updateProduct: jasmine.createSpy('updateProduct').and.returnValue(of({})),
+      createProduct: jasmine.createSpy('createProduct').and.returnValue(of({})),
+      deleteProduct: jasmine.createSpy('deleteProduct').and.returnValue(of({})),
+    };
 
+    mockDialog = {
+      open: jasmine.createSpy('open').and.returnValue({
+        afterClosed: () => of({ type: component.typeEdit, product: { id: 1, name: 'Product 1', description: 'Description 1', price: 100, stock: 10 } })
+      })
+    };
+
+    await TestBed.configureTestingModule({
+      imports: [ProductsComponent], // Importar el componente standalone
+      providers: [
+        { provide: ProductService, useValue: mockProductService },
+        { provide: MatDialog, useValue: mockDialog }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +42,28 @@ describe('ProductsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load products on init', () => {
+    const products = [{ id: 1, name: 'Product 1', description: 'Description 1', price: 100, stock: 10 }];
+    mockProductService.products$ = of(products);
+    component.ngOnInit();
+    expect(component.products).toEqual(products);
+  });
+
+  it('should open modal with correct data', () => {
+    const product = { id: 1, name: 'Product 1', description: 'Description 1', price: 100, stock: 10 };
+    const type = component.typeEdit;
+    component.openModal(product, type);
+    expect(mockDialog.open).toHaveBeenCalledWith(ProductModalComponent, {
+      width: '400px',
+      data: { type, product }
+    });
+  });
+
+  it('should handle modal result correctly', () => {
+    const product = { id: 1, name: 'Product 1', description: 'Description 1', price: 100, stock: 10 };
+    component.openModal(product, component.typeEdit);
+    expect(mockProductService.updateProduct).toHaveBeenCalledWith(product);
   });
 });
